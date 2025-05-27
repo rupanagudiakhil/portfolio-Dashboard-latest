1c244f7b23af541fadf851071b8a4f67cbb15a66
RUPANAGUDI SADIK AHAMED <126375836+rupanagudiakhil@users.noreply.github.com>
2025-05-27 11:16:36 +0530

    removing comments

--- pages/api/google.ts
+++ pages/api/google.ts
@@ -1,12 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { fetchGoogleFinanceData } from '../../lib/googleFinance';
-
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { symbol } = req.query;
-//   const data = await fetchGoogleFinanceData(symbol as string);
-//   res.status(200).json(data);
-// };
-
 import { fetchGoogleFinanceData } from '../../lib/googleFinance';
 import { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
--- pages/api/yahoo.ts
+++ pages/api/yahoo.ts
@@ -1,14 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { fetchYahooFinanceData } from '../../lib/yahooFinance';
-
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { symbol } = req.query;
-//   const cmp = await fetchYahooFinanceData(symbol as string);
-//   res.status(200).json({ cmp });
-// };
-
-
-import { NextApiRequest, NextApiResponse } from 'next';
 import { fetchYahooFinanceData } from '../../lib/yahooFinance';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
